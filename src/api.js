004a02bdd891fa2f5abff0c12cb3aec2e6ede103
Basappagari Manohar <manumanu27343@gmail.com>
2025-08-30 09:41:12 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://registrationform-backend-ebaz.onrender.com",
 });
 
 // attach token if present
